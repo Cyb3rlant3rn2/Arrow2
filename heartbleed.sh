6076f5cc39b5ee233c605e08fe180dadb6a942c8
Cyb3rlant3rn2 <64853191+Cyb3rlant3rn2@users.noreply.github.com>
2020-05-06 15:49:22 +0000

    Update heartbleed.sh

--- heartbleed.sh
+++ heartbleed.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+echo "[+]Vulnerable Domains"
 while read line
 do
-echo "[+]Vulnerable Domains
+
 nmap -p 443 --script ssl-heartbleed $line >> output.txt
 nmap -p 443 --script ssl-heartbleed $line > tmp.txt
-if [ $(cat tmp.txt | grep -o "vulnerable") == "vulnerable"]
+if [ $(cat tmp.txt | grep -o "vulnerable") -e "vulnerable" ]
 then
 echo "$line"
 fi
 done < url.txt
-
 
